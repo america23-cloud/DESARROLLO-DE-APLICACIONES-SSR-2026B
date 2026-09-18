99b6f58c37b9e7b34e52a211b2c1e5b66266310a
America Ortiz <ameeog9@gmail.com>
2026-09-18 18:53:02 +0000

    ✨ Feat: Carpeta back-end Set up Express application with routing and error handling

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //Configuración de la carpeta pública para servir archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 //Registramos las rutas de la aplicación
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
