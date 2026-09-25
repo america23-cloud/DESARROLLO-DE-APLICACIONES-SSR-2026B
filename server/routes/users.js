40d2b401d677599957e62c206166f0bd126353f6
America Ortiz <ameeog9@gmail.com>
2026-09-25 18:56:18 +0000

    ✨ Feat: Update nodemon users route response and enhance package.json for nodemon

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+res.send('<h1>Lista de usuarios</h1>');
 });
 
 //module.exports = router;
